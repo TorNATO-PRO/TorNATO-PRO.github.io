c2e6524b8741f1ce61f74746cebe3f8bd0b1b2ce
Nathan Waltz <nathanwaltz@Nathans-MacBook-Pro.local>
2025-02-17 12:19:18 -0700

    Trying another deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import { defineConfig } from "vite";
 export default defineConfig({
     build: {
         emptyOutDir: true,
-        cssMinify: true
+        cssMinify: true,
+        rollupOptions: {
+            main: "./index.html",
+            portfolio: './pages/portfolio.html',
+        }
     },
     plugins: [
     ]
