d4f672db7e67f0f4896356a0ea41c921aa02ce88
Nathan Waltz <nathanwaltz@Nathans-MacBook-Pro.local>
2025-02-18 23:27:42 -0700

    Adding compiled elm animation

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,28 @@
 import { defineConfig } from "vite";
-import { glob } from "glob"
-import path from 'path'
+import { glob } from "glob";
+import path from "path";
 
 export default defineConfig({
-    build: {
-        emptyOutDir: true,
-        clean: true,
-        cssMinify: true,
-        rollupOptions: {
-            input: {
-                // Include all HTML files
-                ...Object.fromEntries(
-                    glob.sync('./**/*.html', {
-                        ignore: ['./node_modules/**', './dist/**']
-                    }).map(file => [
-                        path.relative('.', file).replace('.html', ''),
-                        path.resolve(__dirname, file)
-                    ])
-                ),
-                // Include all JS files from src directory
-                ...Object.fromEntries(
-                    glob.sync('./src/**/*.js', {
-                        ignore: ['./node_modules/**', './dist/**']
-                    }).map(file => [
-                        path.relative('./src', file).replace('.js', ''),
-                        path.resolve(__dirname, file)
-                    ])
-                )
-            }
-        }
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        ...Object.fromEntries(
+          glob.sync("./**/*.html", {
+            ignore: ["./dist/**"],
+          }).map((file) => [
+            path.relative(".", file).replace(".html", ""),
+            path.resolve(__dirname, file),
+          ])
+        ),
+      },
+      output: {
+        // Use a unified naming pattern for all assets.
+        assetFileNames: "assets/[name]-[hash][extname]",
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+      },
     },
-    plugins: []
-})
+  },
+  base: "/",
+});
