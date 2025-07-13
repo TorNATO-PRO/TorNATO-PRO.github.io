fbfb141b68668e2593becce1adb7831f4a13c281
Nathan Waltz <nwaltz@panosoft.com>
2025-07-13 13:59:09 -0700

    Fiftieth try is a charm

--- src/components/PrelineLoader.jsx
+++
@@ -1,9 +0,0 @@
-import { useLayoutEffect } from "preact/hooks";
-
-export default function PrelineLoader() {
-    useLayoutEffect(() => {
-        import("preline");
-    }, []);
-
-    return null;
-}
